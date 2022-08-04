55ac7bfc89713ec1c99276a2f4c75efa5a2b0937
Spencer Rauch <rauchs@gmail.com>
2022-08-04 10:20:25 -0700

    and notes

---
+++ W1D4/notes.py
@@ -0,0 +1,33 @@
+"""
+ternary
+_________ ? _____ : _____
+ conditon   true    false
+
+pip install pipenv (windows)
+pip3 install pipenv (osx)
+run this command ONCE EVER to globally install pipenv
+pipenv is the software that lets us manage virtual envs
+
+pipenv install flask 
+python -m pipenv install flask
+python3 -m pipenv install flask
+run this command to set up virtual env / install flask to it
+
+pipenv shell
+enter the virtual environment
+
+python server.py
+python3 server.py
+launches server once in environment
+
+
+path variables
+a way for us to get information from the client via the url requested
+
+@app.route("/path/path/<variable>/<int:num_var>")
+
+rendering templates:
+import render_template from flask
+must have "templates" folder same level as server
+return render_template('template.html')
+"""
